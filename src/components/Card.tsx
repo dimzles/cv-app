1eb38f7736747520903333b6e5604db4f17c57b7
dimzles <dimzlestv@gmail.com>
2023-09-28 22:11:41 +0100

    Create Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import '../styles/Card.css'
+
+interface CardProps {
+    children: React.ReactNode;
+    style: React.CSSProperties
+}
+
+export default function Card(props: CardProps) {
+    return (
+        <div className="card" style={props.style}>
+            {props.children}
+        </div>
+    )
+}
